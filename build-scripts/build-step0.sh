da62f0112f5dc890d682ecbcfc07fdf9f3c1d8ea
André Vitor <andrevssi@outlook.com>
2026-04-25 08:34:18 -0300

    fix: separate autogen step from wine-tools step

--- build-scripts/build-step0.sh
+++ build-scripts/build-step0.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-git apply "./android/patches/server_protocol.def.patch"
-./autogen.sh
-
 mkdir wine-tools
 cd wine-tools
 ../configure --without-x --without-gstreamer --without-vulkan --without-wayland
